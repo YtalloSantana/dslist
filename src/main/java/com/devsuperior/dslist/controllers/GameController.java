d3e3c8476e198d5eeb1b17512108774afc2f9e86
Ytallo Santana <ytallosantana17@gmail.com>
2023-06-06 16:53:48 -0300

    Database seed update

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -7,22 +7,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-<<<<<<< HEAD
 import com.devsuperior.dslist.dto.GameMinDTO;
-=======
->>>>>>> 5ef03c0c1c9112a231fb273ecb98310cb717e8d6
+
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 
 @RestController
 
-<<<<<<< HEAD
 @RequestMapping( value= "/games")
 public class GameController {
 
 	@Autowired
-
-	private GameService gameService; 
+private GameService gameService; 
 	
 	
 	@GetMapping
@@ -30,24 +26,11 @@ public class GameController {
 		
 		List<GameMinDTO> result=gameService.findAll();
 		
-		return result  ;
 		
-		
-		
-=======
-@RequestMapping(value = "/games")
-public class GameController {
-
-	@Autowired
-	private GameService gameService;
-
-	@GetMapping
-	public List<Game> findAll() {
-
-		List<Game> result = gameService.findAll();
-
 		return result;
+		
 
->>>>>>> 5ef03c0c1c9112a231fb273ecb98310cb717e8d6
+
+
 	}
 }
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -1,10 +1,7 @@
 package com.devsuperior.dslist.services;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.stream.Collectors;
-=======
->>>>>>> 5ef03c0c1c9112a231fb273ecb98310cb717e8d6
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,19 +17,13 @@ public class GameService {
 	private GameRepository gameRepository;
 	
 	
-<<<<<<< HEAD
-	public List<GameMinDTO>  findAll(){
-	List<Game> result= gameRepository.findAll();
-	List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).collect(Collectors.toList());
 
-	
-		return dto;
-=======
-	public List< Game>  findAll(){
+	public List<GameMinDTO>  findAll(){
+		
 	List<Game> result = gameRepository.findAll();
 	List< GameMinDTO> dto= result.stream().map(x -> new GameMinDTO(x)).toList();
 	
-		return result;
->>>>>>> 5ef03c0c1c9112a231fb273ecb98310cb717e8d6
+		return dto;
+
 	}
 }
